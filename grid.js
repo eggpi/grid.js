1cdb821ca3af7407205b4bed5f0578057750d2dc
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2011-12-20 15:21:52 -0200

    Added my name to copyright notice :)

--- grid.js
+++ grid.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) <year> <copyright holders>
+ * Copyright (C) 2011 Guilherme P. Gonçalves (guilherme.p.gonc@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
